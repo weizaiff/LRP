09f4c0812a197fd2fcd6018f038f61eb7349f98a
yifan.le <weizaiff@163.com>
2025-11-11 15:51:31 +0800

    testing: gemma3 save neuron

--- examples/.ipynb_checkpoints/quantized_gemma3_all_token_batch-checkpoint.py
+++ examples/.ipynb_checkpoints/quantized_gemma3_all_token_batch-checkpoint.py
@@ -5,7 +5,7 @@ from transformers import BitsAndBytesConfig
 
 from lxt.efficient import monkey_patch
 from lxt.utils import pdf_heatmap, clean_tokens
-from util import save_grad_info
+from util import save_grad_info, save_per_example_lrp_res
 from tqdm import tqdm
 
 import os
@@ -48,9 +48,16 @@ Answer: According to the text, the Eiffel Tower was built in"""
 prompt = """Context: Mount Everest attracts many climbers, including highly experienced mountaineers. There are two main climbing routes, one approaching the summit from the southeast in Nepal (known as the standard route) and the other from the north in Tibet. While not posing substantial technical climbing challenges on the standard route, Everest presents dangers such as altitude sickness, weather, and wind, as well as hazards from avalanches and the Khumbu Icefall. As of November 2022, 310 people have died on Everest. Over 200 bodies remain on the mountain and have not been removed due to the dangerous conditions. The first recorded efforts to reach Everest's summit were made by British mountaineers. As Nepal did not allow foreigners to enter the country at the time, the British made several attempts on the north ridge route from the Tibetan side. After the first reconnaissance expedition by the British in 1921 reached 7,000 m (22,970 ft) on the North Col, the 1922 expedition pushed the north ridge route up to 8,320 m (27,300 ft), marking the first time a human had climbed above 8,000 m (26,247 ft). The 1924 expedition resulted in one of the greatest mysteries on Everest to this day: George Mallory and Andrew Irvine made a final summit attempt on 8 June but never returned, sparking debate as to whether they were the first to reach the top. Tenzing Norgay and Edmund Hillary made the first documented ascent of Everest in 1953, using the southeast ridge route. Norgay had reached 8,595 m (28,199 ft) the previous year as a member of the 1952 Swiss expedition. The Chinese mountaineering team of Wang Fuzhou, Gonpo, and Qu Yinhua made the first reported ascent of the peak from the north ridge on 25 May 1960. \
 Question: How high did they climb in 1922? According to the text, the 1922 expedition reached 8,"""
 
-prompt = [prompt]*1
+prompt = [prompt]*2
 
 bar = tqdm(total=len(prompt))
+res_map = {}
+# get all parameter name
+# 遍历模型的所有命名参数
+for name, param in model.named_parameters():
+    # 检查参数是否有梯度
+    res_map[name] = []
+        
 for iprompt in prompt:
     
     # get input embeddings so that we can compute gradients w.r.t. input embeddings
@@ -75,7 +82,13 @@ for iprompt in prompt:
     max_logits.backward()
     bar.update(1)
 
+    save_per_example_lrp_res(res_map, model)
+
+    model.zero_grad()
+
 save_grad_info(model, os.path.join(output_dir, 'weight_gradients.json'))
+
+torch.save(os.paht.join(output_dir, 'gemma_lrp.pt'))
 
 # obtain relevance by computing Input * Gradient
 relevance = (input_embeds * input_embeds.grad).float().sum(-1).detach().cpu()[0] # cast to float32 before summation for higher precision
