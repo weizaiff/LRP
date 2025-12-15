8578804a724611e72fdfb2de7d04d74cf7659408
yifan.le <weizaiff@163.com>
2025-12-15 14:53:05 +0800

    testing mmlu/ceval/global_mmlu_vi

--- 20251211_LAPE_EVAL_MMLU.py
+++ 20251211_LAPE_EVAL_MMLU.py
@@ -18,17 +18,75 @@ evalscope eval \
  --generation-config temperature=1,seed=2025,do_sample=false,max_tokens=4,batch_size=8 \
  --limit 5
 
-
+#    --limit 5 \
+# org model 
 lm_eval \
     --model vllm \
     --model_args pretrained=/root/autodl-fs/model_zoo/meta-llama/Llama-2-7b-hf,trust_remote_code=True,dtype=bfloat16 \
     --tasks mmlu \
     --device cuda:0 \
-    --batch_size 8 \
-    --output_path llm_eval/test_mmlu_eval \
-    --limit 5 \
+    --output_path /root/autodl-fs/llm_eval/test_mmlu_eval_org_model \
     --num_fewshot 5 \
     --batch_size auto \
     --gen_kwargs do_sample=false,max_new_tokens=5 \
     --seed 2025 \
     --log_samples
+
+# LAPE
+lm_eval \
+    --model vllm \
+    --model_args pretrained=/root/autodl-tmp/llama2_7b_base_BaseNeuron_BaseMask_lang_en,trust_remote_code=True,dtype=bfloat16 \
+    --tasks mmlu \
+    --device cuda:0 \
+    --output_path /root/autodl-fs/llm_eval/test_mmlu_eval_LAPE_llama2_7b_base_BaseNeuron_BaseMask_lang_en \
+    --num_fewshot 5 \
+    --batch_size auto \
+    --gen_kwargs do_sample=false,max_new_tokens=5 \
+    --seed 2025 \
+    --log_samples
+
+lm_eval \
+    --model vllm \
+    --model_args pretrained=/root/autodl-tmp/llama2_7b_base_BaseNeuron_BaseMask_lang_vi,trust_remote_code=True,dtype=bfloat16 \
+    --tasks mmlu \
+    --device cuda:0 \
+    --output_path /root/autodl-fs/llm_eval/test_mmlu_eval_LAPE_llama2_7b_base_BaseNeuron_BaseMask_lang_vi \
+    --num_fewshot 5 \
+    --batch_size auto \
+    --gen_kwargs do_sample=false,max_new_tokens=5 \
+    --seed 2025 \
+    --log_samples
+
+
+lm_eval \
+    --model vllm \
+    --model_args pretrained=/root/autodl-tmp/llama2_7b_base_BaseNeuron_BaseMask_lang_zh,trust_remote_code=True,dtype=bfloat16 \
+    --tasks mmlu \
+    --device cuda:0 \
+    --output_path /root/autodl-fs/llm_eval/test_mmlu_eval_LAPE_llama2_7b_base_BaseNeuron_BaseMask_lang_zh \
+    --num_fewshot 5 \
+    --batch_size auto \
+    --gen_kwargs do_sample=false,max_new_tokens=5 \
+    --seed 2025 \
+    --log_samples
+
+#LRP
+lm_eval \
+    --model vllm \
+    --model_args pretrained=/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en,trust_remote_code=True,dtype=bfloat16 \
+    --tasks mmlu \
+    --device cuda:0 \
+    --output_path /root/autodl-fs/llm_eval/test_mmlu_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en \
+    --num_fewshot 5 \
+    --batch_size auto \
+    --gen_kwargs do_sample=false,max_new_tokens=5 \
+    --seed 2025 \
+    --log_samples
+
+
+
+
+
+
+
+
---
+++ 20251215_eval-Copy1.sh
@@ -0,0 +1,50 @@
+
+export HF_ENDPOINT=https://hf-mirror.com
+source /etc/network_turbo
+
+# model / task / output_dir / num_shot 
+EXPERIMENTS=(
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_zh' 'ceval-valid' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_zh_ceval' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_zh' 'global_mmlu_full_vi' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_zh_global_mmlu_vi' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_zh' 'mmlu' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_zh_mmlu' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en' 'ceval-valid' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en_ceval' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en' 'global_mmlu_full_vi' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en_global_mmlu_vi' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en' 'mmlu' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_en_mmlu' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_vi' 'ceval-valid' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_vi_ceval' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_vi' 'global_mmlu_full_vi' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_vi_global_mmlu_vi' '5')"
+    "('/root/autodl-tmp/LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_vi' 'mmlu' '/root/autodl-fs/llm_eval/test_eval_LRP_llama2_7b_base_BaseNeuron_BaseMask_lang_vi_mmlu' '5')"
+)
+
+
+
+
+for EXP in "${EXPERIMENTS[@]}"; do
+    # 把字符串转成数组
+    eval "tuple=${EXP}"
+
+    PRETRAINED="${tuple[0]}"
+    TASKS="${tuple[1]}"
+    OUTPUT_PATH="${tuple[2]}"
+    NUM_FEWSHOT="${tuple[3]}"
+
+    echo "PRETRAINED = ${PRETRAINED}"
+    echo "TASKS = ${TASKS}"
+    echo "OUTPUT_PATH = ${OUTPUT_PATH}"
+    echo "NUM_FEWSHOT = ${NUM_FEWSHOT}"
+
+    # ====== 开始跑实验 =======
+    mkdir -p "${OUTPUT_PATH}"
+
+    lm_eval \
+        --model vllm \
+        --model_args pretrained=${PRETRAINED},trust_remote_code=True,dtype=bfloat16 \
+        --tasks ${TASKS} \
+        --device cuda:0 \
+        --output_path ${OUTPUT_PATH} \
+        --num_fewshot ${NUM_FEWSHOT} \
+        --batch_size auto \
+        --gen_kwargs do_sample=false,max_new_tokens=5 \
+        --seed 2025 \
+        --log_samples
+
+done
